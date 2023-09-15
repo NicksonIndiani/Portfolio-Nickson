d54c4d02b02141a3c7c0111a29d3bbf85043d2ef
Nickson Indiani <118377565+NicksonIndiani@users.noreply.github.com>
2023-09-15 01:19:17 -0300

    Atualização

--- src/components/nickson-indiani.jsx
+++ src/components/nickson-indiani.jsx
@@ -1,9 +1,43 @@
-import './universidade.css';
+import './nickson-indiani.css';
 
 const NicksonIndiani = () => {
   return (
-    <div className="universidade">
+    <div className="nickson">
       <h1>Sobre</h1>
+      <p>
+        Olá,
+        <br />
+        Meu nome é Nickson, tenho 24 anos e estou empolgado em compartilhar um
+        pouco sobre mim com vocês. Sou um indivíduo apaixonado e dedicado e uma
+        variedade de habilidades.
+        <br />
+        Desde a infância, desenvolvi um amor profundo por jogos e surgiu a
+        curiosidade de como esses jogos são feitos. Essa paixão moldou muitos
+        aspectos da minha vida e me ensinou a perseverar, aprender e crescer
+        continuamente.
+        <br />
+        Estou me graduando em Engenharia da Computação na Universidade Virtual
+        do Estado de São Paulo - UNIVESP e a previsão para conclusão é no ano de
+        2028.
+        <br />
+        Uma das minhas principais habilidades é Desenvolvimento Web. Ao longo
+        dos anos, aprimorei essa habilidade por meio cursos e o mais efetivo e
+        famoso “mão na massa”. Além disso, sou conhecido por minha capacidade de
+        trabalho em equipe e liderança.
+        <br />
+        Em resumo, sou um indivíduo motivado e comprometido, apaixonado pela
+        programação, graduando em Engenharia da Computação, minhas habilidades
+        incluem HTML, CSS, Javascript, React.Js, Node.Js, Vite, Python e MySQL,
+        e estou ansioso para aplicá-las em desafios futuros e oportunidades de
+        crescimento. Estou aberto a novas experiências e sempre pronto para
+        aprender mais.
+        <br />
+        Obrigado por me dar a oportunidade de compartilhar um pouco sobre mim.{' '}
+        <br />
+        Atenciosamente,
+        <br />
+        Nickson Indiani.
+      </p>
     </div>
   );
 };
